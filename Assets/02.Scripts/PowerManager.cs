0aae683bb135587441562f91766b6a284efe9a0d
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-10-29 14:26:38 +0900

    change distance

--- Assets/02.Scripts/PowerManager.cs
+++ Assets/02.Scripts/PowerManager.cs
@@ -8,14 +8,14 @@ public class PowerManager : MonoBehaviour
     private RectTransform rectComponent;
     [SerializeField]
     protected Image imageComp;
-
+    float maxDistance;
 
     // Use this for initialization
     void Start()
     {
         rectComponent = GetComponent<RectTransform>();
         imageComp = rectComponent.GetComponent<Image>();
-        imageComp.fillAmount = (PhotonManager.Instance.lange/15);
+        imageComp.fillAmount = (PhotonManager.Instance.lange/maxDistance);
     }
 
     // Update is called once per frame
